6d8f240f61373ac05c364747813a3d7671a80281
Eric Fuentes <efuentesrico@gmail.com>
2019-11-19 17:15:19 +0000

    Tristeza

---
+++ calor.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+
+
+
+
+
+int main(void)
+{
+    float dt = 0.01;
+    double D = 1.0;
+    double s = 1.0;
+    int N_x = 30;
+    float dx = (1-(-1))/N_x;
+    
+    float t,t_f=0,1;
+    int N_t=dt*(t_f-t);
+    
+    double mesh[N_x][N_x];
+    
+    double G[N_x**2][N_x**2];
+    
+    for (int k)
+    
+    for (int n=0; n<N_t;n_t++){
+        
+    }
+
+    for (int ix=1; ix<nX-1; ix++){
+        Psi[ix][0]=100;
+    }
+    
+    for (int i=0; ix<nX-1; ix++){
+        Psi[ix][0]=100;
+    }
+    return 0;
+}
+
